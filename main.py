dc3920e3bd724fc89bc2c6f0ff9cd54186794ec0
Tommi Alajoki <tommi@alajoki.fi>
2018-11-16 01:23:25 +0200

    Initial stages of a GUI.

--- main.py
+++ main.py
@@ -4,5 +4,30 @@ Created on 16 Nov 2018
 @author: Lyrete
 '''
 
-if __name__ == '__main__':
-    pass
+from appJar import gui
+from dataHandler import dataHandler;
+from game import Game;
+import datetime;
+
+db = dataHandler();
+
+app = gui();
+
+app.addLabel("title", "FIFA tracker v0.1")
+app.setLabelBg("title", "red")
+
+app.addLabelEntry("Goals For")
+app.addLabelEntry("Goals Against")
+
+def press(button):
+    if button == "Close":
+        app.stop();
+    if button == "Submit":
+        goalsFor = app.getEntry("Goals For");
+        goalsAgainst = app.getEntry("Goals Against");
+        newGame = Game(datetime.datetime.now(),goalsFor, goalsAgainst);
+        db.writeToDB(newGame);
+        
+app.addButtons(["Submit", "Close"], press)
+
+app.go()
