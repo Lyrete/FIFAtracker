c02a8aa7ca302aafe92c618d8aa7e7bafd4a2ca5
Tommi Alajoki <tommi@alajoki.fi>
2018-11-16 01:02:50 +0200

    storage str functiostorage str functionn

--- game.py
+++ game.py
@@ -4,20 +4,28 @@ Created on 16 Nov 2018
 @author: Lyrete
 '''
 
-import datetime;
-
 class Game(object):
 
-    def __init__(self, goalsFor, goalsAgainst):
+    def __init__(self, date, goalsFor, goalsAgainst, extraTime = False, pen = False, change = 0):
         self.goalsFor = goalsFor;
         self.goalsAgainst = goalsAgainst;
-        self.date = datetime.datetime.now();
+        self.extraTime = extraTime;
+        self.date = date;
+        self.pen = pen;
+        self.change = change;
+        self.result = self.makeResult();
         
     def makeResult(self):
         if(self.goalsFor > self.goalsAgainst):
-            self.result = "W"
-        elif(self.goalsFor < self.goalsAgaint):
-            self.result = "L"
+            return "W"
+        elif(self.goalsFor < self.goalsAgainst):
+            return "L"
         else:
-            self.result = "D"
+            return "D"
+        
+    def storage(self):
+        return self.date.strftime("%Y-%m-%d") + "," + self.goalsFor + "," + self.goalsAgainst + "\n";
+            
+    def __str__(self):
+        return str(self.result) + " ("+ self.goalsFor + "-" + self.goalsAgainst + ")";
         
